399d206b08648fcaf1749f0874d71441951950b2
Alfredo Bejarano Jaramillo <fredbejara@gmail.com>
2025-03-30 22:19:19 +0200

    Resolvi consultas desde la 6 a la 8

--- proyectos_sql/consultas_actuaciondb.sql
+++ proyectos_sql/consultas_actuaciondb.sql
@@ -30,25 +30,25 @@ WHERE c.id_plataforma = 'MIVI';
 
 
 
-/*3. Contar cuantas series de genero ROMANTICA hay en la plataforma MIVI*/
+/*3. Contar cuantas series de genero romántica hay en la plataforma MIVI*/
 
-SELECT COUNT(categoria)
+SELECT COUNT(categoria) as 'Num. series romanticas'
 FROM cartelera 
 WHERE id_plataforma = 'MIVI' 
 AND categoria = 'romantica';
 
-+------------------+
-| COUNT(categoria) |
-+------------------+
-|                1 |
-+------------------+
++------------------------+
+| Num. series romanticas |
++------------------------+
+|                      1 |
++------------------------+
 1 row in set (0.000 sec)
 
 
 
 /*4. Mostrar el nombre de las series que ha protagonizado “Brad Pitt”*/
 
-SELECT a.nombre_act, s.nombre_serie
+SELECT a.nombre_act AS 'Nombre actor / actriz', s.nombre_serie AS 'Nombre de serie'
 FROM series s
 INNER JOIN actores a ON s.actor_prota = a.actor_prota
 WHERE a.nombre_act = 'Brad Pitt';
@@ -63,30 +63,82 @@ WHERE a.nombre_act = 'Brad Pitt';
 
 
 /*5. Mostrar el nombre del actor/actriz más viejo que ha protagonizado una serie de más de 4 temporadas*/
-SELECT a.nombre_act, MAX(a.edad) AS edad, s.temporadas
+SELECT a.nombre_act AS 'Nombre actor / actriz', MAX(a.edad) AS edad, s.temporadas
 FROM series s
 INNER JOIN actores a ON s.actor_prota = a.actor_prota
 GROUP BY a.edad
 HAVING s.temporadas > 4;
 
-+---------------+------+------------+
-| nombre_act    | edad | temporadas |
-+---------------+------+------------+
-| Daniel Br├╝hl  |   43 |          5 |
-| Javier Bardem |   52 |          8 |
-| Daniel Craig  |   53 |         10 |
-| Brad Pitt     |   58 |          5 |
-+---------------+------+------------+
++-----------------------+------+------------+
+| Nombre actor / actriz | edad | temporadas |
++-----------------------+------+------------+
+| Daniel Br├╝hl          |   43 |          5 |
+| Javier Bardem         |   52 |          8 |
+| Daniel Craig          |   53 |         10 |
+| Brad Pitt             |   58 |          5 |
++-----------------------+------+------------+
 4 rows in set (0.000 sec)
 
 
 /* 6. Mostrar el nombre de la serie con más temporadas emitida en Netflix.*/
+SELECT s.nombre_serie AS 'Serie con + temporadas', MAX(s.temporadas) AS 'Netflix'
+FROM series s
+INNER JOIN cartelera c ON s.cod_serie = c.cod_serie
+WHERE c.id_plataforma = 'NETF';
+
++------------------------+---------+
+| Serie con + temporadas | Netflix |
++------------------------+---------+
+| Breaking Bad           |       8 |
++------------------------+---------+
+1 row in set (0.000 sec)
 
 
 
+/* 7. Mostrar todos los datos de los actores Españoles con más de 15 películas que tengan menos de 60 años.*/
+/*Corregí en BBDD la ñ -> EspaNa ya que MariaDB no lo reconoce*/
 
-7. Mostrar todos los datos de los actores Españoles con más de 15 películas que tengan menos de 45 años. 
-8. Mostrar el nombre de la serie con más temporadas con una duración mayor de 60 
+SELECT nombre_act AS 'nombre', nacionalidad, edad, num_peliculas
+FROM actores 
+WHERE nacionalidad = 'Espana'
+GROUP BY nombre_act 
+HAVING edad < 60 AND num_peliculas > 15;               
+
++----------------+--------------+------+---------------+
+| nombre         | nacionalidad | edad | num_peliculas |
++----------------+--------------+------+---------------+
+| Javier Bardem  | Espana       |   52 |            45 |
+| Pen├®lope Cruz  | Espana       |   47 |            40 |
++----------------+--------------+------+---------------+
+2 rows in set (0.000 sec)
+
+
+/* Si quisiera que el numero de peliculas sea mayor a 41 */
+/* HAVING edad < 60 AND num_peliculas > 41; */
+
++---------------+--------------+------+---------------+
+| nombre        | nacionalidad | edad | num_peliculas |
++---------------+--------------+------+---------------+
+| Javier Bardem | Espana       |   52 |            45 |
++---------------+--------------+------+---------------+
+1 row in set (0.000 sec)
+
+
+/*8. Mostrar el nombre de la serie con más temporadas con una duración mayor de 60 */ 
+
+SELECT nombre_serie, MAX(temporadas) AS 'Mas temporadas', duracion
+FROM series
+WHERE duracion > 60;
+
++-----------------+----------------+----------+
+| nombre_serie    | Mas temporadas | duracion |
++-----------------+----------------+----------+
+| Game of Thrones |              8 |      100 |
++-----------------+----------------+----------+
+1 row in set (0.000 sec)
+
+
+
 9. Mostrar el nombre y el argumento de las series emitidas en HBOM 
 10. Mostrar el total de las series que protagonice “Leonardo Di Caprio” 
 11. Calcular la media del precio de las plataformas “Semestrales” 
